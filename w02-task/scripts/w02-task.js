3337447ad82fd6eb624184672b60776189f5fe95
Lemonpathic <124409140+Lemonpathic@users.noreply.github.com>
2024-01-14 14:38:14 -0700

    finished sats project

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,44 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Bryan Wilson";
+let currentYear = new Date().getFullYear();
+let profilePicture = 'images/1698700189209.jpg';
+let altProfilePicture = 'images/placeholder.png';
+
+let singleFood = 'Chicken Legs';
+let foodArray = ['Chicken', 'Chicken Tenders', 'Chicken Nuggies', "Chicken Fingers", "Chicken Wings", "Chicken Breast"];
+foodArray.push(singleFood);
+foodArray.shift();
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const profileElement = document.getElementById("home").querySelector("picture").querySelector("img");
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentYear}`;
+profileElement.setAttribute('src', profilePicture);
+profileElement.setAttribute('alt', altProfilePicture);
+foodElement.innerText = `${foodArray.join(", ")}`;
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
